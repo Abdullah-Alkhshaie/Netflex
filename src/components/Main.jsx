4c133fea6c04865128ccc471f16e2ea0031995cf
abdullah-alkhshaie <abdullahalkhshaie@gmail.com>
2024-01-01 17:31:25 +0300

    update the main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,6 +12,7 @@ function Main() {
   const [movies, setMovies] = React.useState([]);
   const [statu, setStatu] = React.useState("idle");
   const [error, setError] = React.useState(null);
+  const [currentMovie, setCurrentMovie] = React.useState(null);
 
   const sliderRef = useRef(null);
 
@@ -43,19 +44,25 @@ function Main() {
   const settings = {
     dots: false,
     infinite: true,
-    speed: 15000,
+    speed: 600,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 0,
+    autoplaySpeed: 10000, // Set to 9000 milliseconds (9 seconds)
     cssEase: "linear",
     beforeChange: (oldIndex, newIndex) => {
-      // Handle slide change if needed
+      setCurrentMovie(movies[newIndex]);
     },
+    prevArrow: <></>, // Hide default previous arrow
+    nextArrow: <></>, // Hide default next arrow
   };
 
   if (statu === "loading") {
-    return <p>Loading...</p>;
+    return (
+      <div className="w-full h-[650px] mb-20 text-white flex items-center justify-center">
+        <FaSpinner size={50} className="animate-spin" />
+      </div>
+    );
   }
 
   if (statu === "failed") {
@@ -63,54 +70,56 @@ function Main() {
   }
 
   return (
-    <div className="w-full h-[650px] mb-20 text-white relative">
+    <div className="relative">
       <MdChevronLeft
         onClick={showPrevMovie}
         size={50}
-        className="absolute top-[30%] rounded-full text-white opacity-50 cursor-pointer left-2 z-10 cursor-pointers "
+        className="absolute top-1/2 left-2 transform -translate-y-1/2 rounded-full text-white opacity-50 cursor-pointer z-10 hover:opacity-100 transition-opacity duration-300"
       />
-      <Slider ref={sliderRef} {...settings}>
-        <div key={currentMovie.id} className="w-full h-full">
-          <div className=" bg-gradient-to-t from-gray-900  absolute w-full h-[650px] "></div>
-          <img
-            className="w-full h-full object-cover  "
-            src={`https://image.tmdb.org/t/p/original/${currentMovie?.backdrop_path}`}
-            alt={currentMovie?.title}
-          />
-          <div className=" absolute w-full bottom-[30%]  px-4  md:p-8 ">
-            <h1 className="  text-3xl md:text-5xl  font-bold ">
-              {currentMovie?.title}
-            </h1>
-            <div className="my-4">
-              <Link
-                onClick={() => window.scrollTo(0, 0)}
-                to={`/movieCate/${currentMovie.id}`}
-              >
-                <button className="bg-gray-300 text-black py-2 px-5 border-gray-300 rounded text-lg mr-4">
-                  Play
-                </button>
-              </Link>
-              <button className=" bg-transparent border border-gray-300 py-2 px-5 ">
-                Watch Later
-              </button>
-            </div>
 
-            <p className=" text-xs text-gray-400 ">
-              Released {currentMovie?.release_date}
-            </p>
-            <p className="w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] ">
-              {currentMovie?.overview.length > 150
-                ? currentMovie?.overview.slice(0, 150) + "..."
-                : currentMovie?.overview}
-            </p>
+      <Slider ref={sliderRef} {...settings}>
+        {movies.map((movie) => (
+          <div key={movie.id} className="relative">
+            <div className="absolute inset-0 bg-gradient-to-t from-gray-900 opacity-70"></div>
+            <img
+              className="w-full h-[650px] object-cover"
+              src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
+              alt={movie?.title}
+            />
+            <div className="absolute inset-0 flex flex-col items-center justify-center text-white">
+              <h1 className="text-3xl md:text-5xl font-bold mb-4">
+                {movie?.title}
+              </h1>
+              <div className="flex gap-4">
+                <Link
+                  onClick={() => window.scrollTo(0, 0)}
+                  to={`/movieCate/${movie.id}`}
+                >
+                  <button className="bg-gray-300 text-black py-2 px-5 border-gray-300 rounded text-lg mr-4">
+                    Play
+                  </button>
+                </Link>
+                <button className="bg-transparent border border-gray-300 py-2 px-5">
+                  Watch Later
+                </button>
+              </div>
+              <p className="text-lg text-gray-400 my-2">
+                Released {movie?.release_date}
+              </p>
+              <p className="w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] leading-6">
+                {movie?.overview.length > 150
+                  ? movie?.overview.slice(0, 250) + "..."
+                  : movie?.overview}
+              </p>
+            </div>
           </div>
-        </div>
+        ))}
       </Slider>
 
       <MdChevronRight
         onClick={showNextMovie}
         size={50}
-        className="absolute top-[30%] rounded-full text-white opacity-50 cursor-pointer right-2 z-10 cursor-pointers "
+        className="absolute top-1/2 right-2 transform -translate-y-1/2 rounded-full text-white opacity-50 cursor-pointer z-10 hover:opacity-100 transition-opacity duration-300"
       />
     </div>
   );
