c5cfc4af4ad30bf18f651e4512cc3d1bd663727e
abdullah-alkhshaie <abdullahalkhshaie@gmail.com>
2024-01-01 18:17:03 +0300

    update main

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/Netflex/",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ["react-slick"],
+    },
+  },
 });
