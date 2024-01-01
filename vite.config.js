7ae7f0b7562b4778557c388a97f69039bc827970
abdullah-alkhshaie <abdullahalkhshaie@gmail.com>
2024-01-01 18:50:51 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path"; // Import the path module
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/Netflex/",
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["react-slick"],
+  resolve: {
+    alias: {
+      "react-slick": path.resolve(__dirname, "path/to/react-slick"),
     },
   },
 });
