5706282f43979e606ba6f6e79afdfcf7a93f0d80
abdullah-alkhshaie <abdullahalkhshaie@gmail.com>
2024-01-01 15:04:39 +0300

    update project with new features

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,18 @@ import Home from "./pages/Home";
 import MovieCate from "./pages/MovieCate";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import SearchResults from "./components/SearchResults";
 
 function App() {
   return (
- <div className="text-white">
+    <div className="text-white">
       <Navbar />
       <Routes basename="/Netflex">
         <Route path="/" element={<Home />} />
         <Route path="/movieCate/:id" element={<MovieCate />} />
         <Route path="/signIn" element={<SignIn />} />
         <Route path="/signUp" element={<SignUp />} />
+        <Route path="/search/:query" element={<SearchResults />} />
       </Routes>
       <Footer />
     </div>
