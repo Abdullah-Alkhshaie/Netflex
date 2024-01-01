c512facaf553533e5cdead976f66825c8e455e93
abdullah-alkhshaie <abdullahalkhshaie@gmail.com>
2024-01-01 18:53:14 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path"; // Import the path module
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "react-slick": path.resolve(__dirname, "path/to/react-slick"),
+      "react-slick": "react-slick/dist/react-slick.min.js",
     },
   },
 });
